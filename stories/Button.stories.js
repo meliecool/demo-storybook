5f2afeb26e7fb714cfc27789799a199badd10599
meliecool <35310834+meliecool@users.noreply.github.com>
2024-04-19 15:36:22 -0400

    Add chromatic + default story

---
+++ stories/Button.stories.js
@@ -0,0 +1,43 @@
+import { fn } from '@storybook/test';
+import { Button } from './Button';
+
+export default {
+  title: 'Button',
+  tags: ['autodocs'],
+  render: (args) => Button(args),
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+  args: { onClick: fn() },
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
